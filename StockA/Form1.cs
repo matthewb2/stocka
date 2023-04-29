93a3678fa29fb8a30db625e13c25a6b07fee7254
matthewb2 <bestsciencekwabe@gmail.com>
2023-04-29 20:28:02 +0900

    v0.3.3

--- StockA/Form1.cs
+++ StockA/Form1.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using XA_DATASETLib;
@@ -27,22 +29,46 @@ namespace StockA
         string password, accno, accpw;
         
         SearchSt sst;
+        public int profit, loss, km;
         public QuickOrd qo;
         public ListCurSt lsc;
+        StockInfo si;
 
+        public string ordermethod;
         Balance bl;
+        private System.Threading.Timer timer;
+
+        
         public Form1()
         {
             InitializeComponent();
 
             
 
+            //환경변수 로드
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+            using (StreamReader r = new StreamReader(path + @"\pref.json"))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+            
+            //조건식 로드
+            this.profit = items[0].profitrate;
+            this.loss = items[0].lossrate;
+            this.ordermethod = items[0].method;
+            this.km = items[0].km;
+
+            logtxtBox.Text += this.ordermethod + Environment.NewLine;
+            logtxtBox.Text += this.km + Environment.NewLine;
 
             //간단주문 준비
             qo = new QuickOrd(logtxtBox, accno, accno);
 
             lsc = new ListCurSt();
-
+            //
+            si = new StockInfo(logtxtBox, listView2);
 
 
             textBox4.Text = "";
@@ -52,23 +78,24 @@ namespace StockA
             checkBox1.Checked = true;
             button2.Enabled = false;
             button3.Enabled = false;
+            button4.Enabled = false;
 
 
             listView1.View = View.Details;
             
             //
             listView1.Columns.Add("추정순자산");
+            listView1.Columns.Add("매입금액");
             listView1.Columns.Add("평가금액");
             listView1.Columns.Add("평가손익");
             listView1.Columns.Add("손익률(%)");
+            listView1.Columns.Add("보유종목수");
             listView1.Columns.Add("당일실현손익");
-            listView1.Columns.Add("매입금액");
-
             listView1.Columns.Add("예탁자산총액");
             listView1.Columns.Add("D+1 예수금");
             listView1.Columns.Add("D+2 예수금");
             
-            listView1.Columns.Add("보유종목수");
+            
             //
             listView1.Columns[0].Width = 300;
             listView1.Columns[0].TextAlign = HorizontalAlignment.Center;
@@ -114,9 +141,18 @@ namespace StockA
 
             SetHeight(listView2, 40);
 
+            listView2.SelectedIndexChanged += listView2_SelectedIndexChanged;
 
+            logtxtBox.Text += getTime() + Environment.NewLine;
             
 
+        }
+
+        public string getQnt(string price, string notes)
+        {
+            int qn = Convert.ToInt32(notes) / Convert.ToInt32(price);
+
+            return qn.ToString();
         }
 
         private void SetHeight(ListView LV, int height)
@@ -143,6 +179,13 @@ namespace StockA
             
         }
 
+
+        private void listView2_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+                                    
+        }
+
+
         private void button1_Click(object sender, EventArgs e)
         {
             id = textBox4.Text;
@@ -153,20 +196,28 @@ namespace StockA
             session = new XASessionClass();
             bool conn  = session.ConnectServer("demo.ebestsec.co.kr", 20001);
             button1.Enabled = false;
+
             //로그인 성공 메시지 수신 후에 보유계좌 수 호출 로그인 이벤트 핸들러를 이용
             session._IXASessionEvents_Event_Login += XASession_Login;
+
 
             if (conn)
                 ((XA_SESSIONLib.IXASession)session).Login(textBox4.Text, textBox2.Text, "", 0, false);
             
         }
 
+        public string getTime()
+        {
+            return DateTime.Now.ToString("hh:mm:ss tt");
+        }
         private void XASession_Login(string szCode, string szMsg)
         {
-            logtxtBox.Text += "로그인 okay" + Environment.NewLine;
+            
+            logtxtBox.Text += getTime()+" 로그인 okay" + Environment.NewLine;
             logged = true;
             if(!button2.Enabled)
                 button2.Enabled = true;
+            button4.Enabled = true;
 
             //잔고
             int nCount = session.GetAccountListCount();
@@ -174,14 +225,11 @@ namespace StockA
             logtxtBox.Text += String.Format("보유계좌수: {0}", nCount) + Environment.NewLine;
 
             bl = new Balance(logtxtBox, listView1, listView2, this.accno, this.accpw);
-
             bl.request();
 
-            
             //test codes here
-            Order or = new Order(logtxtBox, accno, accpw);
-            //or.request("086520", "601000");
-
+            //Order or = new Order(logtxtBox, accno, accpw);
+        
 
         }
 
@@ -189,16 +237,12 @@ namespace StockA
         {
             if (running)
             {
-                MessageBox.Show("자동매매가 실행중입니다. 그래도 종료하시겠습니까", "알림", MessageBoxButtons.YesNo);
-                //MessageBox.Show("예");
                 session.Logout();
                 button2.Enabled = false;
                 button1.Enabled = true;
                 if (button1.Enabled)
                     button4.Enabled = false;
 
-
-                //Application.Exit();
             }
             else
             {   
@@ -218,46 +262,39 @@ namespace StockA
 
         }
 
-        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void listView2_MouseUp(object sender, MouseEventArgs e)
         {
-            //QuickOrd qord = new QuickOrd();
             
             ListViewHitTestInfo HI = listView1.HitTest(e.Location);
+            Point mousePosition = listView2.PointToClient(Control.MousePosition);
+            ListViewHitTestInfo hit = listView2.HitTest(mousePosition);
+            int columnindex = hit.Item.SubItems.IndexOf(hit.SubItem);
+            int rowindex = hit.Item.Index;
+
             if (e.Button == MouseButtons.Right)
             {
                 //마우스 우클릭 현재가 매도
-                Point mousePosition = listView2.PointToClient(Control.MousePosition);
-                ListViewHitTestInfo hit = listView2.HitTest(mousePosition);
-                int columnindex = hit.Item.SubItems.IndexOf(hit.SubItem);
-                int rowindex = hit.Item.Index;
-                //MessageBox.Show(rowindex.ToString());
                 SellHelper sh = new SellHelper(logtxtBox, listView2, accno, accpw);
                 sh.StartPosition = FormStartPosition.Manual;
                 sh.Location = Cursor.Position;
                 sh.Show();
 
-
-
             }
+
 
             // stocks manipulation when a mouse click event occurs
             if (qo.Visible)
             {
-                Point mousePosition = listView2.PointToClient(Control.MousePosition);
-                ListViewHitTestInfo hit = listView2.HitTest(mousePosition);
-                int columnindex = hit.Item.SubItems.IndexOf(hit.SubItem);
-                int rowindex = hit.Item.Index;
                 qo.textBox3.Text = listView2.Items[rowindex].SubItems[1].Text;
                 qo.textBox1.Text = listView2.Items[rowindex].SubItems[2].Text;
                 qo.textBox2.Text = listView2.Items[rowindex].SubItems[4].Text;
                 qo.textBox4.Text = listView2.Items[rowindex].SubItems[0].Text;
 
             }
+            //현재가 종목 정보
+            StockInfo si = new StockInfo(logtxtBox, listView2);
+            si.request(listView2.Items[rowindex].SubItems[0].Text);
             
 
 
@@ -293,27 +330,34 @@ namespace StockA
                 logtxtBox.Text += "환경변수 불러오기를 실패했습니다" + Environment.NewLine;
                 return;
             }
-            //환경변수 로드
-            string[] st = items[0].light;
-            System.Object[] ItemObject = new System.Object[st.Length];
-
-            for (int i = 0; i < st.Length; i++)
+            //익절 또는 손절 조건을 만족하는 보유주식 매도
+            Order od = new Order(logtxtBox, accno, accpw);
+            float yield = this.profit;
+            float negative = this.loss;
+            foreach (ListViewItem itemRow in this.listView2.Items)
             {
-                //ItemObject[i] = String.Format("{0}", st[i]);
+                
+                
+                float ret = float.Parse(itemRow.SubItems[8].Text, CultureInfo.InvariantCulture.NumberFormat);
+                
+                if (ret > yield || ret< negative)
+                {
+                    logtxtBox.Text += String.Format("{0} {1} {2}",itemRow.SubItems[0].Text, itemRow.SubItems[2].Text, itemRow.SubItems[4].Text) + Environment.NewLine;
+
+                    od.request(itemRow.SubItems[0].Text, itemRow.SubItems[2].Text.Replace(",", ""), "1", itemRow.SubItems[4].Text);
+                }
+                
                 
             }
-            foreach (var item in items)
-            {
-                //logtxtBox.Text += item.millis + Environment.NewLine;
-                
-
-            }
-            //실시간 조건검색 로드
-            sst = new SearchSt(logtxtBox, listView2, id, accno, accpw);
-            sst.request();
-            //
-            sst.end();
             
+
+            //실시간 조건검색 로드
+            sst = new SearchSt(logtxtBox, listView2, lsc.listView1, id, accno, accpw, ordermethod, this.km);
+            sst.request();
+            
+            sst.end();
+            // 15:15:00 전량매도
+            SetUpTimer(new TimeSpan(15, 15, 00));
         }
 
         private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
@@ -323,7 +367,7 @@ namespace StockA
 
         private void button3_Click(object sender, EventArgs e)
         {
-            sst.end();
+            
             button3.Enabled = false;
             button2.Enabled = true;
         }
@@ -347,7 +391,8 @@ namespace StockA
 
         private void 정보ToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            About ab = new About();
+                ab.Show();
         }
 
         private void 주문내역확인ToolStripMenuItem_Click(object sender, EventArgs e)
@@ -366,9 +411,6 @@ namespace StockA
                 lsc.Show();
                 lsc.Visible = true;
 
-                //string accno = "55501502101";
-                //string accpw = "0000";
-
                 OrderCur odc = new OrderCur(lsc.listView1, accno, accpw);
                 odc.request();
             }
@@ -381,6 +423,27 @@ namespace StockA
             //About ab = new About();
             //ab.Show();
             bl.request();
+        }
+
+        private void SetUpTimer(TimeSpan alertTime)
+        {
+            DateTime current = DateTime.Now;
+            TimeSpan timeToGo = alertTime - current.TimeOfDay;
+            if (timeToGo < TimeSpan.Zero)
+            {
+                return;//time already passed
+            }
+            this.timer = new System.Threading.Timer(x =>
+            {
+                this.SomeMethodRunsAt1515();
+            }, null, timeToGo, Timeout.InfiniteTimeSpan);
+        }
+
+        private void SomeMethodRunsAt1515()
+        {
+            //this runs at 15:15:00
+            //전량 매도
+            //MessageBox.Show("run");
         }
     }
 
